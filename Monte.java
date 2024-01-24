5b46149fc91ce1e5f353ea384ca57076e14268af
JanekAgent <janek.skowron@outlook.com>
2024-01-24 07:15:23 +0100

    monte better

--- Monte.java
+++ Monte.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class NaivePlayer extends Player {
 
     public static void main(String[] args) {}
-    private static final int MAX_ITERATIONS = 10;
+    private static final int MAX_ITERATIONS = 100;
     private static final double UCT_CONSTANT = 1.414;
     // monte carlo tree search
     private Random random = new Random(0xdeadbeef);
@@ -23,6 +23,7 @@ public class NaivePlayer extends Player {
     private long timeOfRound;
     private Player.Color oponentColor;
     private Node root;
+    
     private class Node {
         Board board;
         Move move;
@@ -31,12 +32,18 @@ public class NaivePlayer extends Player {
         int winCount;
         int visitCount;
         int wining;
+        Color moveOf;
     
 
         Node(Board board, Move move, Node parent) {
             this.board = board;
             this.move = move;
             this.parent = parent;
+            if (parent != null) {
+                this.moveOf = getOpponent(parent.moveOf);
+            } else {
+                this.moveOf = getColor();
+            }
             this.children = new ArrayList<>();
             this.wining = 0;
         }
@@ -57,36 +64,44 @@ public class NaivePlayer extends Player {
     }
 
     // Główna metoda MCTS
-    public void MTSC(Board currentBoard, Color playerColor) {
+    public void MTSC(Board currentBoard) {
         
-
+        
         for (int i = 0; i < MAX_ITERATIONS; i++) {
+            
+            
             Node selectedNode = selectNode(root);
             expansion(selectedNode);
-            int result = simulateRandomPlayout(selectedNode, playerColor);  
+            Node nodeToExplore = selectedNode.children.get(random.nextInt(selectedNode.children.size()));
+            int result = simulateRandomPlayout(selectedNode);  
             backPropagate(selectedNode, result);
         }
 
        
     }
-    public int simulateRandomPlayout(Node node, Color playerColor) {
+    public int simulateRandomPlayout(Node node) {
         Node tempNode = node;
         Board tempBoard = tempNode.board.clone();
         int boardSize = tempBoard.getSize();
-        int tempColor = playerColor.ordinal();
-        int tempOpponentColor = oponentColor.ordinal();
+        Color tempColor = tempNode.moveOf;
+        Color pponentColor = getOpponent(getColor());
+        
         int tempResult = 0;
-        while (tempBoard.getWinner(playerColor) == null) {
-            List<Move> moves = tempBoard.getMovesFor(playerColor);
+        while (tempBoard.getWinner(getColor()) == null ) {
+            List<Move> moves = tempBoard.getMovesFor(tempColor);
             tempBoard.doMove(moves.get(random.nextInt(moves.size())));
-            tempColor = (tempColor + 1) % 2;
-            tempOpponentColor = (tempOpponentColor + 1) % 2;
+            if (tempColor == getColor()) {
+                tempColor = getOpponent(getColor());
+            } else {
+                tempColor = getColor();
+            }
+            
         }
-        if (tempBoard.getWinner(playerColor) == playerColor) {
+        if (tempBoard.getWinner(tempColor) == getColor()) {
             tempResult = 1;
-        } else if (tempBoard.getWinner(playerColor) == oponentColor) {
+        } else if (tempBoard.getWinner(tempColor) == oponentColor) {
             tempResult = 0;
-        }
+        } 
         return tempResult;
     }
     
@@ -120,10 +135,12 @@ public class NaivePlayer extends Player {
                     .max((node1, node2) -> Double.compare(node1.uctValue(), node2.uctValue()))
                     .orElseThrow(IllegalStateException::new);
         }
+        // System.out.println("koniec wyboru");
+        // System.out.println(node);
         return node;
     }
     private void expansion(Node node) {
-        List<Move> moves = node.board.getMovesFor(getColor());
+        List<Move> moves = node.board.getMovesFor(node.moveOf);
         for (Move move : moves) {
             Board tempBoard = node.board.clone();
             tempBoard.doMove(move);
@@ -159,20 +176,37 @@ public class NaivePlayer extends Player {
             timeOfRound = this.getTime();
             oponentColor = getOpponent(getColor());
             root = new Node(b, null, null);
+            
         }
         for (Node child : root.children) {
-            if (child.board.equals(b)) {
+            if (child.board.getMovesFor(getColor())==b.getMovesFor(getColor())) {
                 root = child;
                 root.parent = null;
+                System.out.println();
                 break;
             }
         }
         
-        MTSC(b, getColor());
+        MTSC(b);
         System.out.println("hello");
         Move bestMove = getBestMove(root);
         System.out.println(root);
+        System.out.println(root.winCount);
+        System.out.println(root.visitCount);
         System.out.println("mefe");
+        if (bestMove == null || !b.getMovesFor(getColor()).contains(bestMove)) {
+            System.out.println("Blad");
+            System.out.println(bestMove);
+            for (Node child : root.children) {
+                System.out.println(child.move);
+            }
+            List<Move> moves = b.getMovesFor(getColor());
+            bestMove = moves.get(random.nextInt(moves.size()));
+            
+            firstRund = true;
+            return bestMove;
+        }
+        System.out.println(getColor());
         Board whatNext = b.clone();
         whatNext.doMove(bestMove);
         System.out.println("Maslo");
